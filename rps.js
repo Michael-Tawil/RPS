686fbd087a324372692a129ee8054c26bedb7151
Michael-Tawil <michaeltawil3@gmail.com>
2022-07-10 21:09:14 +1000

    rps commit intial

---
+++ rps.js
@@ -0,0 +1,17 @@
+let ComputerSelection = ["R", "P","S"];
+
+function ComputerPlay(){
+      return ComputerSelection[Math.floor(Math.random()*ComputerSelection.length)];
+}
+
+function PlayerSelection(){
+
+      
+}
+
+function PlayRound(ComputerSelection,PlayerSelection){
+
+      if (PlayerSelection == "")
+
+}
+console.log(ComputerPlay());
