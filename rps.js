e38740c68102c150771a8e460008d2bf56dfc4f8
Michael-Tawil <michaeltawil3@gmail.com>
2022-07-13 06:22:21 +1000

    Added final score feature

--- rps.js
+++ rps.js
@@ -1,5 +1,7 @@
 let ComputerSelection = ["R", "P","S"];
 let PlayerSelection ;
+let Playerscore = 0;
+let Computerscore= 0;
 
 function ComputerPlay(){
 
@@ -17,10 +19,12 @@ function PlayRound(PlayerSelection,ComputerSelection){
             else if(PlayerSelection == "P" || PlayerSelection == "p"){
 
                   alert("You Win, computer picked Rock");
+                  Playerscore++;
             }
             else if(PlayerSelection == "S" || PlayerSelection == "s"){
 
                   alert("You Lose, computer picked Rock");
+                  Computerscore++;
             }
             else{
                   alert("Selection is invalid");
@@ -35,10 +39,12 @@ function PlayRound(PlayerSelection,ComputerSelection){
             else if(PlayerSelection == "S" || PlayerSelection == "s"){
 
                   alert("You WIN, computer picked Paper");
+                  Playerscore++;
             }
             else if(PlayerSelection == "R" || PlayerSelection == "r"){
 
                   alert("You Lose, computer picked Paper");
+                  Computerscore++;
             }
             else{
                   alert("Selection is invalid");
@@ -53,15 +59,20 @@ function PlayRound(PlayerSelection,ComputerSelection){
             else if(PlayerSelection == "R" || PlayerSelection == "r"){
 
                   alert("You Win, computer picked Scissors");
+                  Playerscore++;
             }
             else if(PlayerSelection == "P" || PlayerSelection == "p"){
 
                   alert("You Lose, computer picked Scissors");
+                  Computerscore++;
             }
             else{
                   alert("Selection is invalid");
             }
       }
+
+      return Playerscore;
+      return Computerscore;
 }
 
 function game(){
@@ -72,8 +83,16 @@ function game(){
       PlayerSelection = prompt("Enter R,P or S");
       PlayRound(PlayerSelection,ComputerSelection)
 
+      }
 
-
+      if ( Computerscore > Playerscore){
+            alert("Computer Won With " + Computerscore + " Points");
+      }
+      else if(Playerscore > Computerscore){
+            alert("You Won With " + Playerscore + " Points");
+      }
+      else{
+            alert("Invalid Score");
       }
 }
 
